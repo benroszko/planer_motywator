8bf3924428e6d3d5c8990b64f78b6965630c7438
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2020-06-01 21:59:42 +0200

    Pug template engine and friends data

--- app.js
+++ app.js
@@ -5,19 +5,25 @@ const router = express.Router();
 
 
 app.use(express.static(__dirname + '/app'));
+app.set('view engine', 'pug')
+
+
+friends = [{nick:'Jan', lvl:2, img:'../resources/picture.svg'},
+           {nick:'Sześcian', lvl:8, img:'../resources/picture.svg'}, 
+           {nick:'Ośmiościan', lvl:11, img:'../resources/picture.svg'}]
 
 const views_path = __dirname + '/app/views/'
 
 router.get('/planer',function(req,res){
-  res.sendFile(path.join(views_path + 'planer.html'));
+  res.render(path.join(views_path + 'planer'), {})
 });
 
 router.get('/znajomi',function(req,res){
-  res.sendFile(path.join(views_path + 'znajomi.html'));
+  res.render(path.join(views_path + 'znajomi'), { friends: friends})
 });
 
 router.get('/widok_dnia',function(req,res){
-  res.sendFile(path.join(views_path + 'widok_dnia.html'));
+  res.render(path.join(views_path + 'widok_dnia'), {})
 });
 
 //add the router
