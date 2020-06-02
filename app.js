52210b559db4a2a31a440c0f802b81efaf39de76
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2020-06-03 00:32:01 +0200

    fixed css issue

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app/'));
+
 app.set('view engine', 'pug')
 const views_path = __dirname + '/app/views/'
 
 
-users = [{nick:'Jan', lvl:2, img:'../resources/picture.svg', friend:true},
-            {nick:'Sześcian', lvl:8, img:'../resources/picture.svg', friend:true}, 
-            {nick:'Ośmiościan', lvl:11, img:'../resources/picture.svg', friend:true},
-            {nick:'Pjoter', lvl:2, img:'../resources/picture.svg', friend:false},
-            {nick:'Osa', lvl:8, img:'../resources/picture.svg', friend:false}]
+users = [{nick:'Jan', lvl:2, img:'resources/picture.svg', friend:true},
+            {nick:'Sześcian', lvl:8, img:'resources/picture.svg', friend:true}, 
+            {nick:'Ośmiościan', lvl:11, img:'resources/picture.svg', friend:true},
+            {nick:'Pjoter', lvl:2, img:'resources/picture.svg', friend:false},
+            {nick:'Osa', lvl:8, img:'resources/picture.svg', friend:false}]
 
 days = [
   {
